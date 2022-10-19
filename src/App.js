4a95869944c2d6e1fe58ece771369b136a1a93c2
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-10-19 19:40:17 +0300

    github linked

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <p>Hello world</p>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project is coded by
+        <a href="(#)"> Niki Farkas </a> and {""}
+        <a href="https://github.com/NikiWolf93/weather-app-week5">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
