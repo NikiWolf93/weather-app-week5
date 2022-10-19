fcd327e6ff9a0b0473306f9f6479eb773278ffea
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-10-19 20:07:31 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project is coded by
-        <a href="(#)"> Niki Farkas </a> and {""}
-        <a href="https://github.com/NikiWolf93/weather-app-week5">
-          open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          {" "}
+          This project is coded by
+          <a href="(#)"> Niki Farkas </a> and {""}
+          <a href="https://github.com/NikiWolf93/weather-app-week5">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
