525b664e68001bd70523fb0bb5783bbe760eae88
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-10-22 11:15:42 +0300

    API integration

--- src/Weather.js
+++ src/Weather.js
@@ -3,16 +3,26 @@ import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
 
   function handleResponse(response) {
     console.log(response.data);
-    setTemperature(response.data.main.temp);
-    setReady(true);
+
+    setWeatherData({
+      ready: true,
+      date: "Sunday 12:00",
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      perception: 2,
+
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
@@ -34,31 +44,33 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>Turku</h1>
+        <h1>{weatherData.city}</h1>
 
         <ul>
-          <li>Wednesday 20:00</li>
-          <li>Mostly cloudy</li>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
 
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
               <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                src={weatherData.iconUrl}
                 className="float-right"
-                alt="mostly cloudy"
+                alt={weatherData.description}
               />
 
-              <span className="Temperature">{Math.round(temperature)}</span>
+              <span className="Temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
               <span className="unit">°C</span>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              <li>Perception 2%</li>
-              <li>Humidity: 88%</li>
-              <li>Wind: 2m/s</li>
+              <li>Perception: {weatherData.perception} %</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}m/s</li>
             </ul>
           </div>
         </div>
